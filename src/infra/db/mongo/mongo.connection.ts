4bb43b3fff0359122553dcc97938b281cbaaf886
viniciuslunardi <vinilunardi00@gmail.com>
2025-05-21 10:29:11 -0300

    fix: fixing husky config, testing (3)

--- src/infra/db/mongo/mongo.connection.ts
+++ src/infra/db/mongo/mongo.connection.ts
@@ -3,7 +3,7 @@ import { MongoConfig } from '../../../types/config.types';
 import logger from '../../../shared/logger';
 
 export async function connectDb(config: MongoConfig) {
-    const uri = `mongodb://${config.user}:${config.password}@${config.uri}:${config.port}/${config.db}?authSource=${config.auth}`;
+const uri = `mongodb://${config.user}:${config.password}@${config.uri}:${config.port}/${config.db}?authSource=${config.auth}`;
 try {
     await mongoose.connect(uri);
     logger.info(`Connected to db sucessfully`, { label: 'db' });
